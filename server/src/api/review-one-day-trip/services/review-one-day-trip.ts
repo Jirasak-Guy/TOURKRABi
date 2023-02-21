c0c66d50ed9d5b3ef3edd1e37103708e999e55ea
จิรศักดิ์ ทรงศรี <jirasak.s.guy@gmail.com>
2023-02-22 01:47:25 +0700

    Create Review One Day Trip

---
+++ server/src/api/review-one-day-trip/services/review-one-day-trip.ts
@@ -0,0 +1,7 @@
+/**
+ * review-one-day-trip service
+ */
+
+import { factories } from '@strapi/strapi';
+
+export default factories.createCoreService('api::review-one-day-trip.review-one-day-trip');
