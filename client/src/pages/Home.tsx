feb049394adab479e2bf51ac6c82e6e424c37132
psu6510110166 <6510110166@psu.ac.th>
2023-02-22 13:09:08 +0700

    Create header(navbar) + menu + submenu + css

--- client/src/pages/Home.tsx
+++ client/src/pages/Home.tsx
@@ -3,6 +3,29 @@ import './Home.css';
 function Home() {
     return (
         <div className="page-container">
+            <header className="nav-bar">
+                <nav className="nav-box-left">
+                    <ul className="menu-left">
+                        <li><a href='#section01' >หน้าหลัก</a></li>
+                        <li>
+                            <a href='#select' className='select'>แพ็คเกจ</a>
+                            <div className="sub-menu">
+                                <ul>
+                                    <li><a href='#onedaytrip'>One day trip</a></li>
+                                    <li><a href='#package'>แพ็คเกจพร้อมที่พัก</a></li>
+                                </ul>
+                            </div>
+                        </li>
+                    </ul>
+                </nav>
+                <div className="nav-box-center" />
+                <nav className="nav-box-right">
+                    <ul className="menu-right">
+                        <li><a href='#login'  >ลงชื่อเข้าใช้</a></li>
+                        <li><a href='#register' >สมัครสมาชิก</a></li>
+                    </ul>
+                </nav>
+            </header>
             <section id="section01" className="box1-container">
                 <div className="box1-image-left" />
                 <div className="box1-image-center" />
@@ -13,8 +36,8 @@ function Home() {
                 </div>
                 <a href="#section02" ><span></span><span></span><span></span></a>
             </section >
-        </div>
-    );
+        </div >
+    )
 };
 
 export default Home;
