edde4952201dd7bb737286570579a3c7a4bf2a27
psu6510110166 <6510110166@psu.ac.th>
2023-02-22 13:17:01 +0700

    Create footer for homepage

--- client/src/pages/Home.tsx
+++ client/src/pages/Home.tsx
@@ -62,6 +62,13 @@ function Home() {
                     </a>
                 </div>
             </section>
+            <div className="footer-container">
+                <div className="footer">
+                    <p>Copyright</p>
+                    <div className='copyright-image' />
+                    <p>2023 TOURKRABi</p>
+                </div>
+            </div>
         </div >
     )
 };
