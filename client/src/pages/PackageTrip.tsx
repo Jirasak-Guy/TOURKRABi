817e27756b2181cb32ffd6a7d78033a1a92e7deb
ปุญญพัฒน์ แก้วขาว <aimaster0010@gmail.com>
2023-02-24 06:36:19 +0700

    change packagetrip(mock) to call data from backend

--- client/src/pages/PackageTrip.tsx
+++ client/src/pages/PackageTrip.tsx
@@ -1,20 +1,21 @@
 import { Box, TextField, InputAdornment, Grid } from "@mui/material";
 import { Search } from '@mui/icons-material';
 import AppBar from "../components/myAppBar";
-import { useEffect ,useState, ChangeEvent } from 'react';
+import { useEffect, useState, ChangeEvent } from 'react';
 import PKT from '../models/PKT';
 import { PackageTripRepo, PKTFilter } from '../repositories/PackageTripRepo'
 import '../App.css'
 
-import ODTCard from '../components/ODTCard'
+import PKTCard from '../components/PKTCard'
 
-function  PackageTrip() {
+function PackageTrip() {
   const [PackageTrip, setPackageTrip] = useState<PKT[]>([])
   const [searchFilter, setSearchFilter] = useState('')
 
-  const onUpdatePackagetrip = (PackageTrip : PKT) => {
-    setPackageTrip(prevPackageTrip => 
-      prevPackageTrip.map(item => item.id === PackageTrip.id ? PackageTrip : item))}
+  const onUpdatePackagetrip = (PackageTrip: PKT) => {
+    setPackageTrip(prevPackageTrip =>
+      prevPackageTrip.map(item => item.id === PackageTrip.id ? PackageTrip : item))
+  }
 
   const fetchODT = async () => {
     const filter: PKTFilter = {
@@ -32,54 +33,57 @@ function  PackageTrip() {
   }
 
   const filteredTrips = PackageTrip.filter(trip =>
-    trip.tourname.toLowerCase().includes(searchFilter.toLowerCase())
+    trip.attributes.tourname.toLowerCase().includes(searchFilter.toLowerCase())
   );
-  
+
   useEffect(() => {
     fetchODT()
-  },[searchFilter])
+  }, [searchFilter])
 
   return (
     <Box>
       <AppBar></AppBar>
       <Box>
-        <img src="OneDayTrip.png" width={"100%"}/>
+        <img src="PackageTrip.png" width={"100%"} />
       </Box>
-      <Box 
-      sx={{ '& > :not(style)': 
-      { m: 2, ml:5 ,minWidth:'120px' ,width:'30%'},
-      '@media(max-width:450px)':
-        {'& > :not(style)': {
-          width: '75%',
+      <Box
+        sx={{
+          '& > :not(style)':
+            { m: 2, ml: 5, minWidth: '120px', width: '30%' },
+          '@media(max-width:450px)':
+          {
+            '& > :not(style)': {
+              width: '75%',
+            }
+          }
         }}
-      }} 
       >
-        <TextField 
-        className='SearchFilter' 
-        label="Search" 
-        variant='outlined' 
-        value={searchFilter} 
-        onChange={handleChangeSearchFilter} 
-        InputProps={{
-        endAdornment: (
-          <InputAdornment position="end">
-            <Search fontSize="large"/>
-          </InputAdornment>
-        ),
-        }}
-      />
+        <TextField
+          className='SearchFilter'
+          label="Search"
+          variant='outlined'
+          value={searchFilter}
+          onChange={handleChangeSearchFilter}
+          InputProps={{
+            endAdornment: (
+              <InputAdornment position="end">
+                <Search fontSize="large" />
+              </InputAdornment>
+            ),
+          }}
+        />
       </Box>
       <div>
-        <Grid 
-          container 
-          spacing={{ xs: 2, md: 3 }} 
-          columns={{ xs: 1, sm: 2, md: 12, lg: 12, xl: 6}}
+        <Grid
+          container
+          spacing={{ xs: 2, md: 3 }}
+          columns={{ xs: 1, sm: 2, md: 12, lg: 12, xl: 6 }}
         >
-        {filteredTrips.map((ODT, index) => (
-          <Grid item xs={2} sm={4} md={4} lg={3} xl={2} key={index}>
-            <ODTCard onedaytrip={ODT} onUpdateOnedaytrip={onUpdatePackagetrip}/>
-          </Grid>
-        ))}
+          {filteredTrips.map((PKT, index) => (
+            <Grid item xs={2} sm={4} md={4} lg={3} xl={2} key={index}>
+              <PKTCard packageTrip={PKT} onUpdatepackageTrip={onUpdatePackagetrip} />
+            </Grid>
+          ))}
         </Grid>
       </div>
     </Box>
