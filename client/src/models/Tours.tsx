941f66a9977dfab3b527b4f653dfaf028095d7e9
ปุญญพัฒน์ แก้วขาว <aimaster0010@gmail.com>
2023-03-01 00:33:22 +0700

    Change ODT & PKT model to Tours model

---
+++ client/src/models/Tours.tsx
@@ -0,0 +1,28 @@
+export default interface Tour {
+    id: number
+    attributes: {
+        tour_name: string
+        current_participate: number
+        maximun_participate: number
+        tour_detial: string
+        tour_type: string
+        tour_image: {
+            data: [
+                {
+                    attributes: {
+                        url: string
+                    }
+                }
+            ]
+        }
+        price_onedaytrip?: {
+            price: number
+        }
+        price_package?: [
+            {
+                room_class_hotel: string
+                price: number
+            }
+        ]
+    }
+}
