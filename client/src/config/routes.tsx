8dbca370fbd90240cdf81575868b500d85c44cd8
ปุญญพัฒน์ แก้วขาว <aimaster0010@gmail.com>
2023-03-01 00:48:50 +0700

    change routes for tours pages

--- client/src/config/routes.tsx
+++ client/src/config/routes.tsx
@@ -1,16 +1,15 @@
 import { Route, Routes } from "react-router-dom";
 import Home from "../pages/Home";
-import OneDayTrip from "../pages/OneDayTrip";
 import Tourreview from "../pages/TourReview";
-import PackageTrip from "../pages/PackageTrip";
+import Tour from "../pages/Tours";
 
 const AppRoutes = () => {
     return (
         <Routes>
             <Route index element={<Home />} />
-            <Route path="onedaytrip" element={<OneDayTrip />} />
-            <Route path="onedaytrip/:id" element={<Tourreview />} />
-            <Route path="packagetrip" element={<PackageTrip />} />
+            <Route path="onedaytrip" element={<Tour />} />
+            <Route path="tour/:id" element={<Tourreview />} />
+            <Route path="packagetrip" element={<Tour />} />
         </Routes>
     );
 };
