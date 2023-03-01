96f73372433daf976e6fa5378b91f06b167faf93
psu6510110166 <6510110166@psu.ac.th>
2023-03-01 18:39:54 +0700

    Add Check the calid input

--- client/src/components/signupform.tsx
+++ client/src/components/signupform.tsx
@@ -13,7 +13,7 @@ const initialUser = { username: "", email: "", password: "" };
 
 function SignupPopup(props: SignupPopupProps) {
 
-    const [user, setUser] = useState(initialUser)
+    const [user, setUser] = useState(initialUser);
 
     const handleChange = ({ target }: React.ChangeEvent<HTMLInputElement>) => {
         const { name, value } = target;
@@ -23,12 +23,43 @@ function SignupPopup(props: SignupPopupProps) {
         }));
     }
 
+    const isValidEmail = (email: string) => {
+        const re = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
+        return re.test(String(email).toLowerCase());
+    };
+
+    const validateInputs = () => {
+        const usernameValue = user.username.trim();
+        const emailValue = user.email.trim();
+        const passwordValue = user.password.trim();
+
+        let errorMessage: string = "";
+
+
+        if (usernameValue === '') {
+            errorMessage += 'Username is required.\n';
+        }
+
+        if (emailValue === '') {
+            errorMessage += 'Email is required.\n';
+        } else if (!isValidEmail(emailValue)) {
+            errorMessage += 'Provide a valid email address.\n'
+        }
+
+        if (passwordValue === '') {
+            errorMessage += 'Password is required.\n'
+        } else if (passwordValue.length < 6) {
+            errorMessage += 'Password must be at least 6 characters.\n'
+        }
+        return errorMessage
+    };
+
     const handleSignup = async () => {
         const url = 'http://localhost:1338/api/auth/local/register'
         try {
             if (user.username && user.password && user.email) {
-                const res = await axios.post(url, user)
-                if (res) {
+                const result = await axios.post(url, user)
+                if (result) {
                     setUser(initialUser)
                     props.onClose()
                     Swal.fire(
@@ -36,9 +67,21 @@ function SignupPopup(props: SignupPopupProps) {
                         'ยินดีต้อนรับสู่เว็บ TOURKRABi ของเรา'
                     )
                 }
+            } else if (!user.username && !user.password && !user.email) {
+                alert('Please complete the information.')
+            } else {
+                alert(validateInputs());
             }
         } catch (error: any) {
-            console.log(error)
+            if (error.response) {
+                const { data } = error.response;
+                if (data.error.message) {
+                    const errorMessage = data.error.message.toLowerCase();
+                    alert(errorMessage)
+                }
+            } else {
+                console.log(error);
+            }
         }
     }
 
@@ -47,26 +90,28 @@ function SignupPopup(props: SignupPopupProps) {
             <div className="popup-overlay-sigup" onClick={props.onClose} />
             <div className="image-sigup" />
             <div className="popup-container-sigup">
-                <div className="box-container-sigup">
+                <div id="form" className="sigup-form">
                     <h2>สมัครสมาชิก</h2>
-                    <div className="username-box-sigup">
-                        <h4>ชื่อผู้ใช้</h4>
-                        <input type="text" id="username" name='username' value={user.username} placeholder="ชื่อผู้ใช้ของคุณ" className='input-box-sigup' onChange={handleChange} />
+                    <div className="input-control">
+                        <label htmlFor="username">ชื่อผู้ใช้</label>
+                        <input id="username" name="username" type="text" value={user.username} placeholder="ชื่อผู้ใช้ของคุณ" className='input-box-sigup' onChange={handleChange} />
+                        <div className="error"></div>
                     </div>
-                    <div className="email-box-sigup">
-                        <h4>อีเมล</h4>
-                        <input type="email" id="email" name='email' value={user.email} placeholder="อีเมลของคุณ" className='input-box-sigup' onChange={handleChange} />
+                    <div className="input-control">
+                        <label htmlFor="email">อีเมล</label>
+                        <input id="email" name="email" type="text" value={user.email} placeholder="อีเมลของคุณ" className='input-box-sigup' onChange={handleChange} />
+                        <div className="error"></div>
                     </div>
-                    <div className="password-box-sigup">
-                        <h4>รหัสผ่าน</h4>
-                        <input type="password" id="password" name='password' value={user.password} placeholder="รหัสผ่านของคุณ" className='input-box-sigup' onChange={handleChange} />
+                    <div className="input-control">
+                        <label htmlFor="password">รหัสผ่าน</label>
+                        <input id="password" name="password" type="password" value={user.password} placeholder="รหัสผ่านของคุณ" className='input-box-sigup' onChange={handleChange} />
+                        <div className="error"></div>
                     </div>
                     <button type="submit" className="submit-button-sigup" onClick={handleSignup}>สมัครสมาชิก</button>
                     <label>มีบัญชีผู้ใช้อยู่แล้ว? <a href='#login' onClick={props.onLoginLinkClick}>ลงชื่อเข้าใช้</a></label>
                     <div className="google-login">
-                        <label>หรือ</label>
                     </div>
-                </div>
+                </ div>
             </div>
         </div>
     );
