c0c60f8dc125df4f870fd9cfd0540bb517ea7ca2
psu6510110166 <6510110166@psu.ac.th>
2023-03-01 15:52:44 +0700

    Create profile page

--- client/src/config/routes.tsx
+++ client/src/config/routes.tsx
@@ -2,14 +2,16 @@ import { Route, Routes } from "react-router-dom";
 import Home from "../pages/Home";
 import Tourreview from "../pages/TourReview";
 import Tour from "../pages/Tours";
+import Profilepage from "../pages/Profile";
 
 const AppRoutes = () => {
     return (
         <Routes>
             <Route index element={<Home />} />
             <Route path="onedaytrip" element={<Tour />} />
-            <Route path="tour/:id" element={<Tourreview />} />
             <Route path="packagetrip" element={<Tour />} />
+            <Route path="tour/:id" element={<Tourreview />} />
+            <Route path="profile" element={<Profilepage />} />
         </Routes>
     );
 };
---
+++ client/src/pages/Profile.tsx
@@ -0,0 +1,12 @@
+import Appbar from "../components/myAppBar";
+
+function Profilepage() {
+    return (
+        <div>
+            <Appbar></Appbar>
+            <h2>profile</h2>
+        </div>
+    )
+}
+
+export default Profilepage;
