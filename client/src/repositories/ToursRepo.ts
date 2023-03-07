4e973562342c42ab85a3c931b298af3736e3c5d8
จิรศักดิ์ ทรงศรี <jirasak.s.guy@gmail.com>
2023-03-07 20:05:00 +0700

    Add method get

--- client/src/repositories/ToursRepo.ts
+++ client/src/repositories/ToursRepo.ts
@@ -7,9 +7,15 @@ export interface TFilter {
 
 export class TourRepo implements IRepository<Tour> {
   urlPrefix = "http://localhost:1338/api/tours"
-  
+
   async getAll(filter?: TFilter): Promise<Tour[] | null> {
     const resp = await fetch(`${this.urlPrefix}?populate=*`)
+    const data = await resp.json()
+    return data.data
+  }
+
+  async get(id: string): Promise<Tour | null> {
+    const resp = await fetch(`${this.urlPrefix}/${id}?populate=*`)
     const data = await resp.json()
     return data.data
   }
