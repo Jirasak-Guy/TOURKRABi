a7a6b6d2324406e788faeead2e32910dd207970f
Arinchai <izarinchai@gmail.com>
2023-03-07 17:17:38 +0700

    Create Profile.tsx

--- client/src/pages/Profile.tsx
+++ client/src/pages/Profile.tsx
@@ -1,10 +1,58 @@
 import Appbar from "../components/myAppBar";
+import "./Profile.css"
+
+
 
 function Profilepage() {
     return (
         <div>
             <Appbar></Appbar>
-            <h2>profile</h2>
+                <div className="Box">
+                    <div className="Profile-Card">
+                        <div className="username"></div>
+                        <div className="email"></div>
+                    </div>
+                    <div className="History-Booking">
+                        <h1>ประวัติการจอง</h1>
+                    </div>
+                        <table>
+                            <tr>
+                                <th>ชื่อโปรแกรม</th>
+                                <th>จำนวน</th>
+                                <th>วันที่จอง</th>
+                                <th>ราคา</th>
+                                <th>สถานะการจอง</th>
+                            </tr>
+                            <tr>
+                                <td></td>
+                                <td></td>
+                                <td></td>
+                                <td></td>
+                                <td></td>
+                            </tr>
+                            <tr>
+                                <td></td>
+                                <td></td>
+                                <td></td>
+                                <td></td>
+                                <td></td>
+                            </tr>
+                            <tr>
+                                <td></td>
+                                <td></td>
+                                <td></td>
+                                <td></td>
+                                <td></td>
+                            </tr>
+                            <tr>
+                                <td></td>
+                                <td></td>
+                                <td></td>
+                                <td></td>
+                                <td></td>
+                            </tr>
+                        </table>
+                </div>
         </div>
     )
 }
