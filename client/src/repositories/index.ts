1c4bc3fd6bd350e1191c450dc747802353385c4d
จิรศักดิ์ ทรงศรี <jirasak.s.guy@gmail.com>
2023-03-07 21:13:00 +0700

    Add ReviewRepo

--- client/src/repositories/index.ts
+++ client/src/repositories/index.ts
@@ -1,7 +1,9 @@
 import { TourRepo } from "./ToursRepo";
+import { ReviewRepo } from "./ReviewRepo";
 
 const repositories = {
-    TourRepo: new TourRepo()
+    TourRepo: new TourRepo(),
+    ReviewRepo: new ReviewRepo()
 }
 
 export default repositories
