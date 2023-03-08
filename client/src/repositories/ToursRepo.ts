09ce665db324b457a728a37e1dd313be8c0b5262
จิรศักดิ์ ทรงศรี <jirasak.s.guy@gmail.com>
2023-03-09 04:50:32 +0700

    Create getReview method

--- client/src/repositories/ToursRepo.ts
+++ client/src/repositories/ToursRepo.ts
@@ -22,4 +22,10 @@ export class TourRepo implements IRepository<Tour> {
     const data = await resp.json()
     return data.data
   }
+
+  async getReview(id: string): Promise<Tour | null> {
+    const resp = await fetch(`http://localhost:1338/api/tours/${id}?populate[reviews][populate]=author`)
+    const data = await resp.json()
+    return data.data
+  }
 }
