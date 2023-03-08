3e36e9dfd6eece292dcb8513aea006b4ed6ddfce
จิรศักดิ์ ทรงศรี <jirasak.s.guy@gmail.com>
2023-03-09 04:51:20 +0700

    Add UserRepo in index

--- client/src/repositories/index.ts
+++ client/src/repositories/index.ts
@@ -1,9 +1,11 @@
 import { TourRepo } from "./ToursRepo";
 import { ReviewRepo } from "./ReviewRepo";
+import { UserRepo } from "./UserRepo";
 
 const repositories = {
     TourRepo: new TourRepo(),
-    ReviewRepo: new ReviewRepo()
+    ReviewRepo: new ReviewRepo(),
+    UserRepo: new UserRepo()
 }
 
 export default repositories
