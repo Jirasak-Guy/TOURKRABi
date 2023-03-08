a1270fa759cf65b7883438add0fc2a18305cf8de
จิรศักดิ์ ทรงศรี <jirasak.s.guy@gmail.com>
2023-03-09 04:50:52 +0700

    Edit Review Repo

--- client/src/repositories/ReviewRepo.ts
+++ client/src/repositories/ReviewRepo.ts
@@ -13,7 +13,7 @@ export class ReviewRepo implements IRepository<Review> {
     }
 
     async get(id: string): Promise<Review | null> {
-        const resp = await fetch(`http://localhost:1338/api/tours/${id}?populate[reviews][populate]=author`)
+        const resp = await fetch(`http://localhost:1338/api/reviews/${id}?populate[author][populate]=*`)
         const data = await resp.json()
         return data.data
       }
