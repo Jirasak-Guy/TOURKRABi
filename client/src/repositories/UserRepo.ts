6026a6a60a81b4714ba3001bbd3b9e626c6f4f81
จิรศักดิ์ ทรงศรี <jirasak.s.guy@gmail.com>
2023-03-09 04:50:11 +0700

    Create User Repo

---
+++ client/src/repositories/UserRepo.ts
@@ -0,0 +1,18 @@
+import { IRepository } from "./IRepository";
+import UserData from "../models/User";
+
+export class UserRepo implements IRepository<UserData> {
+
+    urlPrefix = "http://localhost:1338/api/users"
+
+    async getAll(): Promise<UserData[] | null> {
+        const resp = await fetch(`${this.urlPrefix}?populate=*`)
+        const data = await resp.json()
+        return data.data
+    }
+    async get(id: string): Promise<UserData | null> {
+        const resp = await fetch(`http://localhost:1338/api/users/${id}?populate=*`)
+        const data = await resp.json()
+        return data.data
+    }
+}
