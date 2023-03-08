403f22c7441f2d9fc7add55ac8e3c9dd56526c2d
จิรศักดิ์ ทรงศรี <jirasak.s.guy@gmail.com>
2023-03-09 04:51:42 +0700

    Edit Review model

--- client/src/models/Reviews.tsx
+++ client/src/models/Reviews.tsx
@@ -1,48 +1,33 @@
 export default interface Review {
     id: number
     attributes: {
-        tour_name: string
-        current_participate: number
-        maximun_participate: number
-        tour_detial: string
-        tour_type: string
-        tour_image: {
-            data: [
-                {
-                    attributes: {
-                        url: string
-                    }
-                }
-            ]
-        }
-        price_onedaytrip?: {
-            price: number
-        }
-        price_package?: [
-            {
-                room_class_hotel: string
-                price: number
-            }
-        ]
-        reviews: {
-            data: [
-                {
-                    id: number
-                    attributes: {
-                        rating: number
-                        comment: string
-                        author: {
-                            data: {
-                                id: number
-                                attributes: {
-                                    username: string,  
+        rating: number
+        comment: string
+        author: {
+            data: {
+                attributes: {
+                    username: string
+                    Avatar: {
+                        data: {
+                            id: string
+                            attributes: {
+                                formats: {
+                                    thumbnail: {
+                                        url: string
+                                    }
                                 }
                             }
                         }
-
                     }
                 }
-            ]
+            }
+        }
+        tour: {
+            data: {
+                attributes: {
+                    id : number
+                }
+            }
         }
     }
 }
