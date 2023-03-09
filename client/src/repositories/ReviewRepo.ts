13f6cb8fa0e176afb6b01ab8c042f412ad659b1c
จิรศักดิ์ ทรงศรี <jirasak.s.guy@gmail.com>
2023-03-09 23:20:53 +0700

    Add Delete in ReviewRepo

--- client/src/repositories/ReviewRepo.ts
+++ client/src/repositories/ReviewRepo.ts
@@ -12,7 +12,7 @@ export class ReviewRepo implements IRepository<Review> {
     urlPrefix = "http://localhost:1338/api/reviews"
     token = getToken();
 
-    async getAll(id : string): Promise<Review[] | null> {
+    async getAll(id: string): Promise<Review[] | null> {
         const resp = await fetch(`${this.urlPrefix}?populate=*`)
         const data = await resp.json()
         return data.data
@@ -22,9 +22,9 @@ export class ReviewRepo implements IRepository<Review> {
         const resp = await fetch(`http://localhost:1338/api/reviews/${id}?populate[author][populate]=*`)
         const data = await resp.json()
         return data.data
-      }
+    }
 
-      async createReview(data: ReviewData): Promise<ReviewData> {
+    async createReview(data: ReviewData): Promise<ReviewData> {
         const resp = await fetch(`${this.urlPrefix}?populate=*`, {
             method: "POST",
             headers: {
@@ -35,6 +35,19 @@ export class ReviewRepo implements IRepository<Review> {
         });
         const data_res = await resp.json()
         return data_res;
+    }
+
+    async deleteReview(id: string): Promise<ReviewData> {
+        const resp = await fetch(`${this.urlPrefix}/${id}`, {
+            method: "DELETE",
+            headers: {
+                "Authorization": `${BEARER} ${this.token}`
+            },
+        });
+        const data_res = await resp.json()
+        return data_res;
+
+        
 
 }
 }
